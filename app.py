ca40a1fbb2176c8dee36b8bfc634bab577b66eec
Mishiesalie <mishiesaliey@gmail.com>
2025-03-04 16:38:17 +0300

    update 2

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from my_app import create_app
+from flask import Flask
 
-app = create_app()
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
